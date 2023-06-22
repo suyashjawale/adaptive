6a50dad440b6a057a3e957d34efdf85a7ddf900d
Suyash Jawale <suyashjawale245@gmail.com>
2023-06-22 09:02:25 +0530

    Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [];
 
 export function getRoutes(): Routes {
-  if (window.innerWidth < 768) {
+  // if (window.innerWidth < 768) {
     return [
       {
         path: '',
@@ -22,31 +22,31 @@ export function getRoutes(): Routes {
         loadComponent: () => import('./components/small/wishlist/wishlist.component').then(mod => mod.WishlistComponent),
       }
     ];
-  }
-  else {
-    return [
-      {
-        path: '',
-        loadComponent: () => import('./components/large/main/main.component').then(mod => mod.MainComponent),
-        children: [
-          {
-            path: '',
-            loadComponent: () => import('./components/large/home/home.component').then(mod => mod.HomeComponent),
-          },
-          {
-            path: 'profile',
-            loadComponent: () => import('./components/large/profile/profile.component').then(mod => mod.ProfileComponent),
-          },
-          {
-            path: 'wishlist',
-            loadComponent: () => import('./components/large/wishlist/wishlist.component').then(mod => mod.WishlistComponent),
-          },
-          {
-            path: 'cart',
-            loadComponent: () => import('./components/large/cart/cart.component').then(mod => mod.CartComponent),
-          }
-        ]
-      }
-    ];
-  }
+  // }
+  // else {
+  //   return [
+  //     {
+  //       path: '',
+  //       loadComponent: () => import('./components/large/main/main.component').then(mod => mod.MainComponent),
+  //       children: [
+  //         {
+  //           path: '',
+  //           loadComponent: () => import('./components/large/home/home.component').then(mod => mod.HomeComponent),
+  //         },
+  //         {
+  //           path: 'profile',
+  //           loadComponent: () => import('./components/large/profile/profile.component').then(mod => mod.ProfileComponent),
+  //         },
+  //         {
+  //           path: 'wishlist',
+  //           loadComponent: () => import('./components/large/wishlist/wishlist.component').then(mod => mod.WishlistComponent),
+  //         },
+  //         {
+  //           path: 'cart',
+  //           loadComponent: () => import('./components/large/cart/cart.component').then(mod => mod.CartComponent),
+  //         }
+  //       ]
+  //     }
+  //   ];
+  // }
 }
