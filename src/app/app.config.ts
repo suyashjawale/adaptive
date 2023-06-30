711d82d519c49b8c081b7cd8b383aa0d69b24a07
Charles Dreiss <cjd82187@gmail.com>
2023-06-30 07:54:09 -0400

    Adding hash location and href
    changed build scrpit to be simpler and different base href

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation, withRouterConfig } from '@angular/router';
 
 import { getRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(getRoutes()) ]
+  providers: [provideRouter(getRoutes(), withHashLocation()) ]
 };
